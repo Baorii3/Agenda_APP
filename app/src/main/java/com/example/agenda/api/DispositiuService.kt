286d0fca87a9b3c1e06d2ba5f1b83a9b52c9aea0
Baorii3 <baorii310@gmail.com>
2026-05-22 14:35:49 +0200

    dispositius

--- app/src/main/java/com/example/agenda/api/DispositiuService.kt
+++ app/src/main/java/com/example/agenda/api/DispositiuService.kt
@@ -4,7 +4,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface DispositiuService {
-    @GET("dispositius/dispositius")
+    @GET("dispositius")
     suspend fun llistaDispositius() : Response<List<DispositiuResponseDto>>
 
 }
